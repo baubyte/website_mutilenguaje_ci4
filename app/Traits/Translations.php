9703505c516f0fcb48ac6292b1daa51126930fcd
baubyte <53358294+baubyte@users.noreply.github.com>
2023-03-25 20:26:16 +0000

    save cache translations

--- app/Traits/Translations.php
+++ app/Traits/Translations.php
@@ -2,35 +2,40 @@
 namespace App\Traits;
 
 use App\Models\Settings\TranslationModel;
+use Exception;
 
 trait Translations{
 
     /**
-     * Undocumented function
+     * Get translation for column
      *
      * @param string $column
      * @param string $default
      * @return string
      */
-    public function translation($column, $default = ''): string{
+    public function translation($column, $default = '', $table = null): string{
+
+        if (!property_exists($this, 'table') && is_null($table)) {
+            throw new Exception('Table property not found or table not passed');
+        }
+        $table = $table ?? $this->table;
         $locale = service('language')->getLocale();
+        $cacheName = "translation_{$table}_{$column}_{$this->attributes['id']}_{$locale}";
 
 
         if($this->locale === $locale){
             return $default;
         }
-
-        $translation = (new TranslationModel)->where([
-            'table' => $this->table,
-            'column' => $column,
-            'table_id' => $this->attributes['id'],
-            'locale' => $locale
-        ])->first();
-        
-        if($translation){
-            return $translation->translation;
-        }else{
-            return $default;
+        if (null === $translation = cache("{$cacheName}")) {
+            $translation = (new TranslationModel)->where([
+                'table' => $table,
+                'column' => $column,
+                'table_id' => $this->attributes['id'],
+                'locale' => $locale
+            ])->get()->getRow();
+            cache()->save("{$cacheName}", $translation, 600);
         }
+
+        return $translation->translation ?? $default;
     }
 }
